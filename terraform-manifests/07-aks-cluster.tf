aa80336792ac4f5d9971c6545f154b1dacc3c044
hkassaoui <57017803+hkassaoui@users.noreply.github.com>
2026-01-02 22:42:13 -0500

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   default_node_pool {
     name       = "systempool"
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "standard_a2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     #availability_zones   = [1, 2, 3]
     # Added June2023
@@ -94,4 +94,5 @@ tags = {
 
 
 }
+
 
